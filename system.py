37a73763f3f8a1a75cda676b0844d41128120315
dimitriy <liman.dimitriy@gmail.com>
2015-03-27 00:09:22 +0200

    first step

---
+++ system.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+import time
+from Queue import Queue
+from threading import Thread
+from random import *
+
+
+class Task (Thread):
+    def __init__(self, target, args):
+        Thread.__init__(target=target, args=args)
+        self.lst_suitable_proc = Task.generate_suitable_proc()
+        self.number_operations = Task.generate_number_operations()
+        # Task.setDaemon(True)      READ DOC ABOUT Daemon threads
+        Task.start()
+
+    @staticmethod
+    def generate_suitable_proc():
+        lst = []
+        for num in xrange(randint(0, 4)):   # if random return 0 -> task empty and won't execute
+            lst.append(randint(0, 4))
+        return lst
+
+    @staticmethod
+    def generate_number_operations(self):   # 200 because 200 ms is max time for execute task,
+        return randint(100,200)             # so the dumbest processor will handle it. 100 - just my random
+
+
+class Processor ():
+    def __init__(self, id, power):
+        self.id = id
+        self.power = power  # power - number of operations per ms
+
+    def worker(self, task_queue):
+        print "start work ", self.id, " processor"
+        time.sleep(task_queue.get().number_operations/(self.power*1000)) # sleep() get in seconds, our task in ms
+        task_queue.task_done()
+
+
+# EXAMPLE OF USING QUEUE
+def do_stuff(q):
+  while True:
+    print q.get()
+    q.task_done()
+
+q = Queue(maxsize=0)
+num_threads = 10
+
+for i in range(num_threads):
+  worker = Thread(target=do_stuff, args=(q,))
+  worker.setDaemon(True)
+  worker.start()
+
+for x in range(20):
+  q.put(x)
+
+q.join()
