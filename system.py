e06390206c44a43548bc58755f71bfb99c5e693e
dimitriy <liman.dimitriy@gmail.com>
2015-03-31 23:52:57 +0300

    looks like planner working; check busy status of processors!

--- system.py
+++ system.py
@@ -14,20 +14,19 @@ def unique(lst):
 
 
 class Task ():
-    def __init__(self):
-        self.lst_suitable_proc = Task.generate_suitable_proc()
-        self.number_operations = Task.generate_number_operations()
+    def __init__(self, *args):
+        self.lst_suitable_proc = Task.generate_suitable_proc(args)
+        self.number_operations = Task.generate_number_operations(args)
 
     @staticmethod
-    def generate_suitable_proc():
+    def generate_suitable_proc(args):
         lst = []
-        for num in xrange(5):   # if random return 0 -> task empty and won't execute
-            if randint(0, 1):
-                lst.append(randint(0, 4))
+        for num in xrange(5):
+            lst.append(randint(0, 4))
         return unique(lst)
 
     @staticmethod
-    def generate_number_operations():   # 200 because 200 ms is max time for execute task,
+    def generate_number_operations(args):   # 200 because 200 ms is max time for execute task,
         return randint(100, 200)             # so the dumbest processor will handle it. 100 - just my random
 
 
@@ -46,10 +45,13 @@ class TaskQueue(Thread):
 
     def run(self):  # fill TaskQueue
         while self.fill_time > self.pass_time(time.time()):
-            self.queueLock.acquire()
+            self.queue_lock.acquire()
+            print "lock queue by Task"
             print "adding task to queue"
-            self.queue.put(Task())
-            print "END adding task to queue"
+            if randint(0, 1):
+                self.queue.put(Task())
+            else:
+                print "task wasn't added"
             self.queue_lock.release()
             time.sleep(0.001)   # every milliseconds adding some task
         else:
@@ -58,13 +60,15 @@ class TaskQueue(Thread):
 
 
 class Processor (Thread):
-    def __init__(self, proc_id, power, manage_queue, control_threads, queue):   # management - name function: FIFO and others algorithms
+    def __init__(self, proc_id, power, manage_queue, queue_lock, cond, queue):   # management - name function: FIFO and others algorithms
         Thread.__init__(self)
         self.id = proc_id
         self.power = power  # power - number of operations per ms
         self.manage_queue = manage_queue
-        self.control = control_threads
+        self.queue_lock = queue_lock
+        self.control = cond
         self.queue = queue
+        self.daemon = True
 
     def run(self):
         while True:
@@ -72,100 +76,119 @@ class Processor (Thread):
 
 
 class WorkerProcessor (Processor):
-    def __init__(self, proc_id, power, manage_queue, control_threads, queue):
-        Processor.__init___(self, proc_id, power, manage_queue, control_threads,queue)
-        self.busy = False
+    def __init__(self, proc_id, power, manage_queue, queue_lock, cond, queue):
+        Processor.__init__(self, proc_id, power, manage_queue, queue_lock, cond, queue)
+        self.task = False
         self.start()
 
     def fifo(self):
         """get first task and try execute by self.id_proc"""
-        self.control.acquire()
+        self.queue_lock.acquire()
         if not self.queue.empty():
             task = self.queue.get()
-            if self.id_proc in task.lst_suitable_proc:
-                print "working ", self.id_proc, " processor"
-                self.control.release()
+            if self.id in task.lst_suitable_proc:
+                print "working ", self.id, " processor. Suitable are", task.lst_suitable_proc
+                self.queue_lock.release()
                 time.sleep(task.number_operations/(self.power*1000))     # sleep() get in seconds, our task in ms
             else:
                 self.queue.put(task)
-                self.control.release()
-                # time.sleep(0.001)       # necessary ???
+                self.queue_lock.release()
         else:
             print "Queue empty"
             self.control.release()
-            time.sleep(0.001)   # time for wedged to queue by TaskQueue
 
     def worker_planner(self):   # control is pair of conditional and lock for queue
         """worker_planner just executing task when condition is send"""
-        self.control[0].acquire()
-        self.control[0].wait()
-        self.control[1].acquire()
-        print "working ", self.id_proc, " processor"
-        self.busy = True
-        task = self.queue.get()
-        self.control[1].release()
-        time.sleep(task.number_operations/(self.power*1000))     # sleep() get in seconds, our task in ms
-        self.control[0].release()
+        print "Lock cond for worker %s" % self.id
+        self.control.acquire()
+        self.control.wait()
+        print "GET NOTIFY in worker", self.id
+        print "task is", self.task
+        time.sleep(self.task.number_operations/(self.power*1000))     # sleep() get in seconds, our task in ms
+        self.task = False
+        self.control.release()
 
 
 class PlanningProcessor (Processor):
-    def __init__(self, proc_id, manage_queue, lst_lock, lst_processors, queue):
-        Processor.__init__(self, proc_id, 0, manage_queue, lst_lock, queue)
-        self.busy = True
+    def __init__(self, proc_id, manage_queue, queue_lock, lst_cond, lst_processors, queue):
+        Processor.__init__(self, proc_id, 0, manage_queue, queue_lock, lst_cond, queue)
+        self.task = True
         self.lst_processors = lst_processors
         self.start()
 
     def planner_helper(self, lst_task):   # return True if sends task to processors and False if doesn't
         task_send = False
-        for id_suitable_proc in lst_task[0].lst_suitable_proc:
-            if not self.lst_processors[id_suitable_proc].busy:
-                map(lambda item: self.queue.put(item), self.lst_processors)
+        print "list of tasks", lst_task
+        print "begin planner helper; finding suitable proc in", lst_task[0].lst_suitable_proc
+        for id_suitable_proc in lst_task[-1].lst_suitable_proc:
+            if not self.lst_processors[id_suitable_proc].task:
+                self.lst_processors[id_suitable_proc].task = lst_task.pop()
                 task_send = True
+                map(lambda item: self.queue.put(item), lst_task)
+                print "find not busy processor", id_suitable_proc, " and sending notify to worker"
                 self.control[id_suitable_proc].acquire()
                 self.control[id_suitable_proc].notify()
                 self.control[id_suitable_proc].release()
-        if not self.workQueue.empty():
+                break
+            else:
+                print "This processor is busy", id_suitable_proc
+        if not self.queue.empty():
             if task_send:
                 return True
             else:
-                self.planner_helper(lst_task.insert(0, self.queue.get()))
+                print "not find free processor for that task"
+                lst_task.append(self.queue.get())
+                self.planner_helper(lst_task)
         else:       # if sort out all queue and won't be free suitable processors. HIGHLY UNLIKELY
-            map(lambda item: self.queue.put(item), self.lst_processors)
+            print "Queue has task only for busy processors"
+            map(lambda item: self.queue.put(item), lst_task)
             return False
 
     def planner(self):      # control is list of conditions and lock for queue
         """planner finds suitable free processors and send condition to work"""
-        self.control[len(self.control)-1].acquire()     # acquire queue
+        self.queue_lock.acquire()
+        print "lock queue by planner"
         if not self.queue.empty():
-            self.planner_helper([self.queue.get()])
-            print "done of planning task"
-            self.control[len(self.control)-1].release()     # release queue
+            task = self.queue.get()
+            if isinstance(task, Task):          # left for debug
+                self.planner_helper([task])
+            else:
+                print "Fucking bug! task is", task
+            print "unlock queue by planner"
+            self.queue_lock.release()     # release queue
+            time.sleep(0.001)
         else:
             print "Queue empty"
-            self.control[len(self.control)-1].release()     # release queue
+            print "unlock queue by planner"
+            self.queue_lock.release()     # release queue
             time.sleep(0.001)   # time for wedged to queue by TaskQueue
 
 
 def main_func_planner(lst_power):
-    lst_cond = [Condition(Lock()) for x in xrange(5)]      # last lock for queue
-    lst_power[lst_power.index(min(lst_power))] = 0
+    lst_cond = [Condition(Lock()) for x in xrange(5)]
+    index_min_elem = lst_power.index(min(lst_power))
+    lst_power[index_min_elem] = 0
     lst_processors = []
     queue_lock = Lock()
     work_queue = Queue()
-    task_queue = TaskQueue(3, queue_lock, work_queue)
+    task_queue = TaskQueue(1, work_queue, queue_lock)
+    queue_lock.acquire()
     for x in xrange(5):
         if lst_power[x] == 0:
             lst_processors.append(PlanningProcessor(x,
                                                     PlanningProcessor.planner,
-                                                    list(set(lst_cond) | set ([queue_lock])),
-                                                    lst_processors,     # not sure that in planner will see all threads
+                                                    queue_lock,
+                                                    lst_cond,
+                                                    lst_processors,
                                                     work_queue))
         else:
             lst_processors.append(WorkerProcessor(x,
                                                   lst_power[x],
                                                   WorkerProcessor.worker_planner,
-                                                  [lst_cond[x], queue_lock],
+                                                  queue_lock,
+                                                  lst_cond[x],
                                                   work_queue))
+    queue_lock.release()
     while not task_queue.done:  # wait for passing 10 seconds
         pass
     print "end of everything!"
@@ -176,17 +199,19 @@ def main_func_fifo(lst_power):
     queue_lock = Lock()
     work_queue = Queue()
     lst_processors = []
-    task_queue = TaskQueue(3, queue_lock, work_queue)      # will append task to queue for 3 seconds, another my random
+    task_queue = TaskQueue(1, work_queue, queue_lock)      # will append task to queue for 3 seconds, another my random
     for x in xrange(5):
         lst_processors.append(WorkerProcessor(x,
                                               lst_power[x],
                                               WorkerProcessor.fifo,
                                               queue_lock,
+                                              None,
                                               work_queue))
     while not task_queue.done:  # wait for passing 10 seconds
         pass
     print "end of everything!"
     return
 
+
 # main_func_fifo([2, 4, 6, 8, 10])
-# main_func_planner([2, 4, 6, 8, 10])
+main_func_planner([2, 4, 6, 8, 10])
